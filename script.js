4ade76d7628985431f94cea469f8274bd29d30ea
Start Coding Student 2 <session2@START-Coding-MBP-C02PP744G8WN.local>
2015-09-30 17:30:10 -0700

    Establishes outline for flow of code.

---
+++ script.js
@@ -0,0 +1,44 @@
+/**
+ * Created by session2 on 9/30/15.
+ */
+
+var character;
+
+var startGame = function(name) {
+    switch(name){
+        case 'Kevin':
+
+            break;
+        case 'Princess Kevin':
+
+            break;
+        case 'Bob':
+
+            break;
+        case 'Nice Queen':
+
+            break;
+        default:
+
+            break;
+    }
+};
+
+var storyKevin = function() {
+
+
+};
+
+var storyKevin = function() {
+
+};
+
+var storyBob = function() {
+
+
+};
+
+var storyQueen = function() {
+
+
+};
