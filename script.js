bf66941f027ecc45dad1ecae0bc43f5cb2f495e4
Taylor Spiller <taylor.spiller@west-mec.org>
2015-10-03 22:07:46 -0700

    End of Saturday changes

--- script.js
+++ script.js
@@ -2,110 +2,155 @@
  * Created by session2 on 9/30/15.
  */
 
-var character;
-var storyLineNumber = 0;
+var character = "Bob";
+var panelNumber = 0;
 var answer;
 
-var storyBob = [ ["Plot line 1", "no1"], ["Plot line 2", 'no2'] ];
-var questionBob = [ ["Does Bob continue to take care of his shop, or does he visit his friend Steve?"] , "Question 2"];
+/**
+* Each panel has one block of storyline text.
+* Incorrect paths take are put in the error array.
+*/
+var storyBob = [ "Plot line 1", "Plot line 2", "Plot line 3" ];
+var errorBob = ["no", "no2", "no3"];
 var storyKevin = [];
-var questionKevin =[];
+var errorKevin = [];
 var storySteve = [];
+var errorSteve = [];
+
+/**
+* Each panel will only have one question. 
+* A question is not needed when the user makes and error and dies.
+*/
+var questionBob = ["Does Bob continue to take care of his shop, or does he visit his friend Steve?" , "Question 2", "Question 3", "question 4"];
+var questionKevin =[];
 var questionSteve = [];
 
 /**
+* Arrays holding the text for the buttons.
+* The arrays are nested. Each panel has an array with position 0 being an array, and position 1 being a string.
+* Ex: for each panel [ ["Button 1 text", "Button 2 text"], "first/second"]
+* The string indicates which button should lead to the next question.
+*/
+var buttonBob = [ [ ["Stay at the Shop", "Visit Steve"], "first" ] , [["Button 2.1", "Button 2.2 True"], "second"] , [ ["Button 3.1 True", "Button 3.2"], "first"] ];
+var buttonKevin = [];
+var buttonSteve = [];
+
+/**
  * Function to change the two paragraphs on the HTML page that display
- * both the storyline text and the question that the player must answer.
+ * both the storyline text and the question that the player must answer, 
+ * the background pictures, and the button text.
  */
-var changeText = function(char){
-    switch(char){
+function nextPanel(){
+			
+    switch(character){
         case 'Bob':
-            document.getElementById("storyText").innerHTML = storyBob[storyLineNumber][0];
-            document.getElementById("questionText").innerHTML = questionBob[storyLineNumber];
+            document.getElementById("storyText").innerHTML = storyBob[panelNumber];
+            document.getElementById("questionText").innerHTML = questionBob[panelNumber];
+			document.getElementById("button1").innerHTML = buttonBob[panelNumber][0][0];
+			document.getElementById("button2").innerHTML = buttonBob[panelNumber][0][1];
+			if(buttonBob[panelNumber][1].toLowerCase() === "first") {	
+				//The first button is the correct path
+				document.getElementById("button1").onclick = nextPanel;
+				document.getElementById("button2").onclick = wrongAnswer;
+			} else if(buttonBob[panelNumber][1].toLowerCase() === "second") {
+				// The second button is the correct path
+				document.getElementById("button2").onclick = nextPanel;
+				document.getElementById("button1").onclick = wrongAnswer;
+				console.log("wrong");
+			} else {
+				// Last panel
+				document.getElementById("button1").onclick = restart;
+				document.getElementById("button1").innerHTML = 
+			}
             break;
 
         case 'Kevin':
-            document.getElementById("storyText").innerHTML = storyKevin[storyLineNumber][0];
-            document.getElementById("questionText").innerHTML = questionKevin[storyLineNumber];
+            document.getElementById("storyText").innerHTML = storyKevin[panelNumber][0];
+            document.getElementById("questionText").innerHTML = questionKevin[panelNumber];
+			document.getElementById("button1").innerHTML = buttonKevin[panelNumber][0][0];
+			document.getElementById("button2").innerHTML = buttonKevin[panelNumber][0][1];
+			if(buttonKevin[panelNumber][1].toLowerCase() === "first") {	
+				//The first button is the correct path
+				document.getElementById("button1").onclick = nextPanel;
+				document.getElementById("button2").onclick = wrongAnswer;
+			} else{
+				// The second button is the correct path
+				document.getElementById("button2").onclick = nextPanel;
+				document.getElementById("button1").onclick = wrongAnswer;
+			}
             break;
 
         case 'Steve':
-            document.getElementById("storyText").innerHTML = storySteve[storyLineNumber][0];
-            document.getElementById("questionText").innerHTML = questionSteve[storyLineNumber];
-            break;
-
-        default:
+            document.getElementById("storyText").innerHTML = storySteve[panelNumber][0];
+            document.getElementById("questionText").innerHTML = questionSteve[panelNumber];
+			document.getElementById("button1").innerHTML = buttonSteve[panelNumber][0][0];
+			document.getElementById("button2").innerHTML = buttonSteve[panelNumber][0][1];
+			if(buttonSteve[panelNumber][1].toLowerCase() === "first") {	
+				//The first button is the correct path
+				document.getElementById("button1").onclick = nextPanel;
+				document.getElementById("button2").onclick = wrongAnswer;
+			} else {
+				// The second button is the correct path
+				document.getElementById("button2").onclick = nextPanel;
+				document.getElementById("button1").onclick = wrongAnswer;
+			}
             break;
     }
-    storyLineNumber ++;
+    panelNumber ++;
 };
 
-var wrongAnswer = function(char){
-    switch(char){
+function wrongAnswer(){
+    switch(character){
         case 'Bob':
-            document.getElementById("storyText").innerHTML = storyBob[storyLineNumber][1];
+            document.getElementById("storyText").innerHTML = errorBob[panelNumber];
             break;
 
         case 'Kevin':
-            document.getElementById("storyText").innerHTML = storyKevin[storyLineNumber][1];
+            document.getElementById("storyText").innerHTML = errorKevin[panelNumber];
             break;
 
         case 'Steve':
-            document.getElementById("storyText").innerHTML = storySteve[storyLineNumber][1];
+            document.getElementById("storyText").innerHTML = errorSteve[panelNumber];
             break;
 
         default:
             break;
     }
-    storyLineNumber = 0;
+	document.getElementById("questionText").innerHTML = "Game Over";
+	document.getElementById("button1").innerHTML = "Play Again";
+	document.getElementById("button2").innerHTML = "";
+	document.getElementById("button1").onclick = restart;
 };
 
 /**
- *Function to start the game. It is called when the start button is clicked.
- * It will call the appropriate function for the character picked.
+ * Function to start the game. It is called when the start button is clicked.
+ * It changes the first picture to the main character and hides the other two pictures.
+ * Then it calls the function to display the next panel.
  */
-var startGame = function(name) {
-    switch(name){
-        case 'Princess Kevin':
-            functionKevin();
-            break;
-        case 'Bob':
-            functionBob();
-            break;
-        case 'Nice Queen':
-            functionSteve();
-            break;
-        default:
-            break;
-    }
+function startGame() {
+	document.getElementById("storyText").innerHTML = "hi";
+	switch(character){
+		case 'Bob':
+			document.getElementById("imgCharacter1").src = "https://derpicdn.net/img/2013/6/28/359217/full.png";
+			break;
+		case 'Steve':
+			document.getElementById("imgCharacter1").src = "http://pre13.deviantart.net/b624/th/pre/i/2014/146/c/e/unicorn_noteworthy_by_vectorizedunicorn-d7ju47d.png";
+			break;
+		case 'Kevin':
+			document.getElementById("imgCharacter1").src = "https://derpicdn.net/img/2013/6/7/342631/full.png";
+			break;
+	}
+	document.getElementById("imgCharacter1").onclick = "";
+	document.getElementById("imgCharacter2").onclick = "";
+	document.getElementById("imgCharacter3").onclick = "";
+	//Hides other characters
+	document.getElementById("imgCharacter2").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Egypt_governorates_blank.png/597px-Egypt_governorates_blank.png";
+	document.getElementById("imgCharacter3").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Egypt_governorates_blank.png/597px-Egypt_governorates_blank.png";
+    nextPanel();
 };
 
-var functionKevin = function() {
-
+function restart(){
+	location.reload();
+	panelNumber = 0;
 };
 
-var functionBob = function() {
-    changeText('Bob');
-
-    /**
-    var answer = prompt("Does Bob continue to take care of his shop, or does he visit his friend Steve?", "Shop or Steve").toLowerCase();
-    switch(answer){
-        case 'shop':
-            confirm("While sweeping the floors that afternoon, Bob hears the sound of glass shattering.");
-            answer = prompt("Does Bob hide or search for the source?", "Hide or Search").toLowerCase();
-            if(answer === "hide")
-            {
-                confirm("Bob peers out from the supply closet to see the royal Princess Kevin frantically searching through his bottles of potions. Many shatter and break in the process. ");
-                confirm('Suddenly Steve breaks through the door. "I need your help, Steve!" He calls. "I need your help in the castle."');
-
-            } else if(answer === "search") {
-                confirm("Standing defenseless in the middle of his shop, Bob catches a glimpse of long purple and yellow striped hair. He slowly strides forward to see the lovely Princess Kevin rifling through his stock of potions. A flash of light is all he sees as he freezes into a block of ice.");
-            }
-            break;
-        case 'steve':
-
-            break;
-        default:
-    }
-     */
-};
